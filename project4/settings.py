87cb0120b6055635e0330eb96475657a59e8f9bd
morphl1n <lukakurdiani@gmail.com>
2023-02-08 23:36:30 +0400

    settings

--- project4/settings.py
+++ project4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
