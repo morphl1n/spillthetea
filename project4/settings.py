a462316a21f921777b1816fdae5c3e67dbd8cba8
morphl1n <lukakurdiani@gmail.com>
2023-02-08 23:22:20 +0400

    added compressor

--- project4/settings.py
+++ project4/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['appspillthetea.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'network',
+    'compressor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
